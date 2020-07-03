302e25dca052d6e2e434c7deefa2b6ad024d9ed6
Luis Felipe <felipedelta0@gmail.com>
2020-07-02 21:30:08 -0300

    Added screens

---
+++ src/screens/Manage/Links/Create/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import Layout from '../../../Layouts/Manage'
+
+const Create = () => {
+  return (
+    <Layout>
+      <h1>Create Link</h1>
+      <div>
+        <form>
+          <div className='form-group'>
+            <label>Label</label>
+            <input type='text' className='form-control' />
+          </div>
+          <div className='form-group'>
+            <label>URL</label>
+            <input type='text' className='form-control' />
+          </div>
+          <div className='form-group form-check'>
+            <label className='form-check-label'>
+              <input type='checkbox' name='isSocial' />
+              <span className='form-check-sign'>Is Social</span>
+            </label>
+          </div>
+          <div>
+            <button className='btn btn-primary btn-round'>Submit</button>
+          </div>
+        </form>
+      </div>
+    </Layout>
+  )
+}
+
+export default Create
