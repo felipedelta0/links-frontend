d78e12a793c63206f6d5c84e41975abcf322a926
Luis Felipe <felipedelta0@gmail.com>
2020-07-02 23:43:20 -0300

    Sign in with api implemented

--- src/screens/SignIn/SignInActions.js
+++ src/screens/SignIn/SignInActions.js
@@ -1,5 +1,9 @@
+import { apiPost } from '../../helpers/api'
+
 export const SIGN_IN = 'SIGN_IN'
 
 export const signInAction = (data) => {
-  return { type: SIGN_IN, payload: data }
+  const payload = apiPost('/auth/signin', data)
+
+  return { type: SIGN_IN, payload }
 }
